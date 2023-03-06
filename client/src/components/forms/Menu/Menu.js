9745b61cd1efdb00ebfda198e04598d0dc609c84
Bektur12 <kanybekovbektur22@gmail.com>
2023-03-07 03:15:09 +0600

    add menu

---
+++ client/src/components/forms/Menu/Menu.js
@@ -0,0 +1,34 @@
+import * as React from "react";
+import { Menu as MUIMenu } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { useState } from "react";
+import MenuItem from "./MenuItem";
+
+function Menu({ options, onHanldeClick, open, onClose }) {
+  return (
+    <>
+      <MUIMenu open={open} onClose={onClose} disableScrollLock>
+        <MenuWrapper>
+          {options.map((option) => (
+            <MenuItem
+              key={Math.random()}
+              icons={option?.icon}
+              onclick={() => {}}
+            >
+              {option.name}
+            </MenuItem>
+          ))}
+        </MenuWrapper>
+      </MUIMenu>
+    </>
+  );
+}
+export default Menu;
+
+const MenuWrapper = styled("div")(() => ({
+  display: "flex",
+  alignItems: "flex-start",
+  flexDirection: "column",
+  rowGap: "16px",
+  padding: "10px 0",
+}));
