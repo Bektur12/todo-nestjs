b827662f9c65cb0b8596fa86da18657adaf6e32d
Bektur12 <kanybekovbektur22@gmail.com>
2023-03-07 03:55:13 +0600

    clear app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,5 @@
 import React from "react";
-import { Button } from "./components/forms/Button/Button";
 
 export const App = () => {
-  return (
-    <div>
-      <Button>hello</Button>
-    </div>
-  );
+  return <div></div>;
 };
