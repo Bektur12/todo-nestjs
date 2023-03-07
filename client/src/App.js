a2d5a35eb9cfea87965a39f46e0165c7fb140585
Bektur12 <kanybekovbektur22@gmail.com>
2023-03-07 22:00:35 +0600

    clear app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,5 @@
 import React from "react";
-import Header from "./components/layout/header/Header";
 
 export const App = () => {
-  return (
-    <div>
-      <Header />
-    </div>
-  );
+  return <div></div>;
 };
